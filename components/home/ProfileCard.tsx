e8366883554283f7786d38a2c1d0f27a57f79ef4
Ahmet ALMAZ <Music47ell@gmail.com>
2022-06-19 13:48:47 +0300

    refactor: remove now playing from profile card refactor: remove motion package

--- components/home/ProfileCard.tsx
+++ components/home/ProfileCard.tsx
@@ -1,77 +1,10 @@
 import { default as Image } from '@/components/Image'
 import { useCallback, useEffect, useRef, useState } from 'react'
-import useSWR from 'swr'
-import fetcher from '@/lib/fetcher'
-import { animate } from 'motion'
-import { NowPlayingSong } from '@/lib/types'
 import { BiBriefcase, BiMap, BiEnvelope, BiLinkExternal } from 'react-icons/bi'
-import { SiSpotify } from 'react-icons/si'
 import siteMetadata from '@/data/siteMetadata'
 import { default as Link } from '@/components/Link'
 
-function AnimatedBars() {
-  useEffect(() => {
-    animate(
-      '#bar1',
-      {
-        transform: [
-          'scaleY(1.0) translateY(0rem)',
-          'scaleY(1.5) translateY(-0.082rem)',
-          'scaleY(1.0) translateY(0rem)',
-        ],
-      },
-      {
-        duration: 1.0,
-        repeat: Infinity,
-        easing: ['ease-in-out'],
-      }
-    )
-    animate(
-      '#bar2',
-      {
-        transform: [
-          'scaleY(1.0) translateY(0rem)',
-          'scaleY(3) translateY(-0.083rem)',
-          'scaleY(1.0) translateY(0rem)',
-        ],
-      },
-      {
-        delay: 0.2,
-        duration: 1.5,
-        repeat: Infinity,
-        easing: ['ease-in-out'],
-      }
-    )
-    animate(
-      '#bar3',
-      {
-        transform: [
-          'scaleY(1.0)  translateY(0rem)',
-          'scaleY(0.5) translateY(0.37rem)',
-          'scaleY(1.0)  translateY(0rem)',
-        ],
-      },
-      {
-        delay: 0.3,
-        duration: 1.5,
-        repeat: Infinity,
-        easing: ['ease-in-out'],
-      }
-    )
-  }, [])
-
-  return (
-    <div className="flex overflow-hidden items-end w-auto">
-      <span id="bar1" className="mr-1 w-1 h-2 bg-gray-300 dark:bg-gray-500 opacity-75" />
-      <span id="bar2" className="mr-1 w-1 h-1 bg-gray-300 dark:bg-gray-500" />
-      <span id="bar3" className="w-1 h-3 bg-gray-300 dark:bg-gray-500 opacity-80" />
-    </div>
-  )
-}
-
 const ProfileCard = (): JSX.Element => {
-  const { data } = useSWR<NowPlayingSong>('/api/now-playing', fetcher)
-
   const ref = useRef(null)
   const [style, setStyle] = useState({})
 
@@ -133,42 +66,11 @@ const ProfileCard = (): JSX.Element => {
               priority={true}
             />
           </div>
-          <Spotify data={data} />
           <ProfileInfo />
           <span className="h-1.5 bg-gradient-to-r from-green-500 via-red-500 to-gray-900"></span>
         </div>
       </div>
     </>
-  )
-}
-
-const Spotify = ({ data }) => {
-  return (
-    <div className="flex items-center py-2 px-3 xl:px-6 bg-gray-800">
-      <SiSpotify className="fill-spotify" />
-      <div className="inline-flex ml-2 truncate">
-        {data?.songUrl ? (
-          <>
-            <div className="flex items-end pt-1 pb-0.5 mr-2 h-5">
-              <AnimatedBars />
-            </div>
-            <a
-              className="font-medium text-gray-200"
-              href={data.songUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              title={`${data.title} - ${data?.artist ?? 'Spotify'}`}
-            >
-              {data.title}
-            </a>
-          </>
-        ) : (
-          <p className="font-medium text-gray-200">Not Playing</p>
-        )}
-        <span className="mx-2 text-gray-300">{' – '}</span>
-        <p className="max-w-max text-gray-300 truncate">{data?.artist ?? 'Spotify'}</p>
-      </div>
-    </div>
   )
 }
 
