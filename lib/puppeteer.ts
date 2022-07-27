27fd20ef85abd66d76aafaebbcec142727600dc0
Ahmet ALMAZ <Music47ell@gmail.com>
2022-07-27 17:54:37 +0300

    refactor: puppeteer code

--- lib/puppeteer.ts
+++ lib/puppeteer.ts
@@ -6,34 +6,21 @@ export const RESUME_URL = process.env.NEXT_PUBLIC_VERCEL_URL
   : 'http://localhost:3000/resume'
 
 async function getOptions() {
-  const isDev = !process.env.AWS_REGION
-  let options
-
-  const chromeExecPaths = {
-    win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-    linux: '/usr/bin/google-chrome',
-    darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  }
-
-  // make ts happy (:
-  if (process.platform !== 'win32' && process.platform !== 'linux' && process.platform !== 'darwin')
-    throw new Error(`current platform (${process.platform}) not supported`)
-
-  const execPath = chromeExecPaths[process.platform]
-
-  if (isDev) {
-    options = {
-      args: [],
-      executablePath: execPath,
-      headless: true,
-    }
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    }
-  }
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === 'win32'
+            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+            : process.platform === 'linux'
+            ? '/usr/bin/google-chrome'
+            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      }
 
   return options
 }
