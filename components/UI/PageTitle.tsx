37b7aeac3f62ddeb00a2b74d43929e9d329bfcef
Ahmet ALMAZ <Music47ell@gmail.com>
2022-11-28 00:56:25 +0300

    chore: same as before

--- components/UI/PageTitle.tsx
+++ components/UI/PageTitle.tsx
@@ -2,10 +2,7 @@ import { IPageTitle } from 'lib/interfaces'
 
 export default function PageTitle({ children }: IPageTitle): JSX.Element {
 	return (
-		<h1
-			itemProp="name headline"
-			className=" text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14"
-		>
+		<h1 className="p-name text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
 			{children}
 		</h1>
 	)
