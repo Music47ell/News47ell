0dfb2daaf2be123ab3d2409c77c37802baac66ab
Ahmet ALMAZ <Music47ell@gmail.com>
2023-03-25 23:00:16 +0300

    chore: remove reactions

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,4 +1,3 @@
 export { default as HeroCard } from './HeroCard'
 export { default as HeroCardLinks } from './HeroCardLinks'
-export { default as TopReactions } from './TopReactions'
 export { default as TopViews } from './TopViews'
--- layouts/HomeLayout.tsx
+++ layouts/HomeLayout.tsx
@@ -1,14 +1,13 @@
-import { HeroCard, HeroCardLinks, TopReactions, TopViews } from '@/components/home'
+import { HeroCard, HeroCardLinks, TopViews } from '@/components/home'
 import { SectionContainer } from '@/components/UI'
 
 // Marquee source: https://codepen.io/ykadosh/pen/KKezJzz
 
 export default function HomeLayout() {
 	return (
-		<SectionContainer>
+		<SectionContainer className="mb-8">
 			<HeroCard />
 			<HeroCardLinks />
-			<TopReactions />
 			<TopViews />
 		</SectionContainer>
 	)
