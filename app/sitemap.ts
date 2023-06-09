11cdd3b4961b6c892768e01416bd66a5f7d69b35
Ahmet ALMAZ <Music47ell@gmail.com>
2023-06-09 10:11:01 +0300

    chore: filter out draft pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,10 +8,12 @@ export default async function sitemap() {
 			url: `${siteMetadata.siteUrl}/blog/${post.slug}`,
 			lastModified: post.updated_at.split('T')[0],
 		}))
-	const pages = allPages.map((post) => ({
-		url: `${siteMetadata.siteUrl}/${post.slug}`,
-		lastModified: post.updated_at.split('T')[0],
-	}))
+	const pages = allPages
+		.filter((post) => !(post.draft === true))
+		.map((post) => ({
+			url: `${siteMetadata.siteUrl}/${post.slug}`,
+			lastModified: post.updated_at.split('T')[0],
+		}))
 
 	const routes = ['', '/blog', '/dashboard'].map((route) => ({
 		url: `${siteMetadata.siteUrl}${route}`,
