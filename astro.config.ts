3cc2534ba9ba390472a5df6e16c6ebfa5520f83f
Ahmet ALMAZ <Music47ell@gmail.com>
2023-12-07 22:05:34 +0300

    feat: switch to vercel
    feat: use vercel edge for analytics
    chore: updates deps
    feat: switch to web component for now playing and watching

--- astro.config.ts
+++ astro.config.ts
@@ -7,14 +7,43 @@ import {
 	remarkReadingTime,
 } from './src/libs/remark'
 import siteMetadata from './src/data/siteMetadata'
-import netlify from '@astrojs/netlify/functions'
 import astroExpressiveCode from 'astro-expressive-code'
+import vercel from '@astrojs/vercel/serverless'
+
+const VERCEL_PREVIEW_SITE =
+	process.env.VERCEL_ENV !== 'production' &&
+	process.env.VERCEL_URL &&
+	`https://${process.env.VERCEL_URL}`
+
+const site = VERCEL_PREVIEW_SITE || siteMetadata.siteUrl
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteMetadata.siteUrl,
+	site: site,
 	output: 'server',
-	adapter: netlify(),
+	server: {
+		headers: {
+			'Accept-Encoding': 'br, gzip, compress',
+			'Content-Security-Policy': 'upgrade-insecure-requests; block-all-mixed-content',
+			'Permissions-Policy':
+				'ambient-light-sensor=(), autoplay=(), accelerometer=(self), camera=(), document-domain=(), encrypted-media=(), fullscreen=(self), geolocation=(), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), speaker=(), sync-xhr=(), usb=(), vr=()',
+			'Referrer-Policy': 'strict-origin-when-cross-origin',
+			'Report-To': `{"group":"default","max_age":31536000,"endpoints":[{"url":"https://news47ell.report-uri.com/a/d/g"}],"include_subdomains":true}`,
+			'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
+			'X-Content-Type-Options': 'nosniff',
+			'X-Frame-Options': 'DENY',
+			'X-XSS-Protection': '1; mode=block',
+		},
+	},
+	adapter: vercel({
+		edgeMiddleware: true,
+		webAnalytics: {
+			enabled: true,
+		},
+		speedInsights: {
+			enabled: true,
+		},
+	}),
 	experimental: {
 		optimizeHoistedScript: true,
 	},
--- src/utils/getFlagEmoji.ts
+++
@@ -1,5 +0,0 @@
-export default function getFlagEmoji(countryCode) {
-	return [...countryCode.toUpperCase()]
-		.map((char) => String.fromCodePoint(127397 + char.charCodeAt()))
-		.reduce((a, b) => `${a}${b}`)
-}
