2d7a637faa66bcbd374c71479fc27dda39d0f002
Ahmet ALMAZ <Music47ell@gmail.com>
2023-12-12 23:20:15 +0300

    fix: test rc version of sharp to fix og api issue

---
+++ src/pages/api/og.png.ts
@@ -0,0 +1,50 @@
+import type { APIRoute } from 'astro'
+import { html } from 'satori-html'
+import satori from 'satori'
+import sharp from 'sharp'
+import siteMetadata from '@/data/siteMetadata'
+
+export async function GET({ request }: APIContext) {
+	const { searchParams } = new URL(request.url)
+	const title = searchParams.has('title') ? searchParams.get('title') : siteMetadata.title
+
+	const url = import.meta.env.PROD === 'production' ? siteMetadata.siteUrl : 'http://localhost:4321'
+
+	const markup =
+		html(`<div tw="flex flex-col justify-center w-full h-full p-12 items-center bg-[#282a36]">
+				<h1 tw="text-5xl text-gray-100 m-auto">${title}</h1>
+				<div tw="flex flex-row items-center">
+					<img src="${`${url}/images/others/me.png`}" tw="rounded-full w-20 h-20 mr-3" alt="${
+						siteMetadata.author.name
+					}" />
+					<span tw="text-5xl text-[#E30A17]">/</span>
+					<span tw="text-3xl text-gray-100">${siteMetadata.title.toLowerCase()} by ${
+						siteMetadata.author.name
+					}</span>
+				</div>
+			</div>`)
+	const svg = await satori(markup, {
+		width: 1200,
+		height: 630,
+		fonts: [
+			{
+				name: 'Alef',
+				data: await fetch('https://fonts.gstatic.com/s/alef/v12/FeVfS0NQpLYgrjJbC5FxxbU.ttf').then(
+					(res) => res.arrayBuffer()
+				),
+				weight: 400,
+				style: 'normal',
+			},
+		],
+	})
+	const png = sharp(Buffer.from(svg)).png()
+	const response = await png.toBuffer()
+
+	return new Response(response, {
+		status: 200,
+		headers: {
+			'Content-Type': 'image/png',
+			'Cache-Control': 's-maxage=1, stale-while-revalidate=59',
+		},
+	})
+}
