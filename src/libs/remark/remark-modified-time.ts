08e1e30ca402d29a4646d6ddfa4da3851591de24
Ahmet ALMAZ <Music47ell@gmail.com>
2024-01-31 21:03:54 +0300

    refactor: lastModified code

--- src/libs/remark/remark-modified-time.ts
+++ src/libs/remark/remark-modified-time.ts
@@ -1,18 +1,9 @@
 import { execSync } from 'child_process'
 
-export default function remarkModifiedTime() {
+export function remarkModifiedTime() {
 	return function (_tree: any, file: any) {
-		try {
-			const filepath = file.history[0]
-			const result = execSync(`git log -1 --pretty="format:%cI" ${filepath}`)
-
-			if (result.toString() === '') {
-				file.data.astro.frontmatter.lastModified = new Date()
-			}
-
-			file.data.astro.frontmatter.lastModified = new Date(result.toString())
-		} catch (e) {
-			console.error(e)
-		}
+		const filepath = file.history[0]
+		const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`)
+		file.data.astro.frontmatter.lastModified = result.toString()
 	}
 }
