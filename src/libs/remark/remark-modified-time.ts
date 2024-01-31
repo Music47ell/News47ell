ddec91fbaa7e39fd8afe986b1a20c8f8128cfa4a
Ahmet ALMAZ <Music47ell@gmail.com>
2024-01-31 23:56:17 +0300

    fix: add published_date in case lastModified isn't available

--- src/libs/remark/remark-modified-time.ts
+++ src/libs/remark/remark-modified-time.ts
@@ -2,8 +2,13 @@ import { execSync } from 'child_process'
 
 export default function remarkModifiedTime() {
 	return function (_tree: any, file: any) {
-		const filepath = file.history[0]
-		const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`)
-		file.data.astro.frontmatter.lastModified = result.toString()
+		try {
+			const filepath = file.history[0]
+			const result = execSync(`git log -1 --pretty="format:%cI" ${filepath}`)
+
+			file.data.astro.frontmatter.lastModified = result.toString()
+		} catch (e) {
+			console.error(e)
+		}
 	}
 }
